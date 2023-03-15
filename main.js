d19f5f41597c577c34b0aba6d23017fa72a5707a
Dmither <mr.d.graumann@gmail.com>
2023-03-15 10:22:18 +0200

    add responsive header

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import './style.scss'
+import './style.scss';
+
+let body = document.querySelector("body");
+let menu = document.querySelector(".menu");
+let burger = document.querySelector(".burger");
+
+burger.addEventListener("click", function(){
+  burger.classList.toggle("burger_active");
+  menu.classList.toggle("menu_active");
+  body.classList.toggle("freeze");
+});
